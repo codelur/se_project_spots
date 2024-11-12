6f352b32d243d783db4fdf507e5997740b1ed373
Sal <codelur@gmail.com>
2024-11-11 16:57:26 -0800

    feat API. initialCards, UserInfo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import Api from "../utils/Api.js";
 import {
   enableValidation,
   settings,
@@ -7,32 +8,30 @@ import {
   enableButton,
 } from "../scripts/validation.js";
 
-const initialCards = [
-  {
-    name: "Desert Castle",
-    link: "https://images.unsplash.com/photo-1725610588150-c4cd8b88affd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+const profileAvatar = document.querySelector(".profile__avatar");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "1f3d47ec-48a6-48e0-95f2-0844dd9a33b4",
+    "Content-Type": "application/json",
   },
-  {
-    name: "Milky Way",
-    link: "https://images.unsplash.com/photo-1725615357444-6123528686cf?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "City Lights",
-    link: "https://plus.unsplash.com/premium_photo-1724458589661-a2f42eb58aca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Beautiful Cliff",
-    link: "https://images.unsplash.com/photo-1535479804851-93f60320e644?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "River Flow",
-    link: "https://images.unsplash.com/photo-1543503484-ba590cb1f903?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Old Town Vernazza",
-    link: "https://images.unsplash.com/photo-1641646936155-c703e5d84b99?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
+});
+
+api
+  .getAppInfo()
+  .then(([cards, userInfo]) => {
+    cards.forEach(function (card) {
+      const cardNode = createCardElement(card.link, card.name);
+      renderCard(cardNode);
+    });
+    profileAvatar.src = userInfo.avatar;
+    profileName.textContent = userInfo.name;
+    profileDescription.textContent = userInfo.about;
+  })
+  .catch(console.error);
 
 const cardTemplate = document.querySelector("#card__template").content;
 const cardsList = document.querySelector(".cards__list");
@@ -76,11 +75,6 @@ const closepreviewImageButton = document.querySelector(
 function renderCard(cardNode, method = "prepend") {
   cardsList[method](cardNode);
 }
-
-initialCards.forEach(function (card) {
-  const cardNode = createCardElement(card.link, card.name);
-  renderCard(cardNode);
-});
 
 function createCardElement(link, name) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
---
+++ src/utils/Api.js
@@ -0,0 +1,53 @@
+// utils/Api.js
+
+class Api {
+  constructor({ baseUrl, headers }) {
+    // constructor body
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  getAppInfo() {
+    return Promise.all([this.getInitialCards(), this.getUserInfo()]);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  // other methods for working with the API
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  editUserInfo({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      // Send the data in the body as a JSON string.
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then((res) => {
+      // handle the response
+    });
+  }
+}
+
+export default Api;
